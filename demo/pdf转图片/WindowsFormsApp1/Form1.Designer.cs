8421f40c2c4237da28c791b37d45a55d53195c8b
贺黎亮 <hell@zhuofan.net>
2020-07-22 09:57:48 +0800

    324324

---
+++ demo/pdf转图片/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.process1 = new System.Diagnostics.Process();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            this.process1.Exited += new System.EventHandler(this.process1_Exited);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(410, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(95, 136);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(271, 39);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "拖放PDF文件到窗口即可开始转换";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "选择pdf文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(145, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "选择转换后保存的文件夹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(183, 225);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "开始转换";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 329);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "M专用pdf转换器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form1_DragOver);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ demo/pdf转图片/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using O2S.Components.PDFRender4NET;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void SetPos(int ipos);
+
+        private void SetTextMessage(int ipos)
+        {
+            if (this.InvokeRequired)
+            {
+                SetPos setpos = new SetPos(SetTextMessage);
+                this.Invoke(setpos, new object[] { ipos});
+            }
+            else
+            {
+                this.label1.Text = ipos.ToString() + "/100";
+                this.progressBar1.Value = Convert.ToInt32(ipos);
+            }
+        }
+
+        public enum Definition
+        {
+            One = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10
+        }
+
+        /// <summary>
+        /// 将PDF文档转换为图片的方法
+        /// </summary>
+        /// <param name="pdfInputPath">PDF文件路径</param>
+        /// <param name="imageOutputPath">图片输出路径</param>
+        /// <param name="imageName">生成图片的名字</param>
+        /// <param name="startPageNum">从PDF文档的第几页开始转换</param>
+        /// <param name="endPageNum">从PDF文档的第几页开始停止转换</param>
+        /// <param name="imageFormat">设置所需图片格式</param>
+        /// <param name="definition">设置图片的清晰度，数字越大越清晰</param>
+        public  void ConvertPDF2Image(string pdfInputPath, string imageOutputPath,
+            string imageName, int startPageNum, int endPageNum, ImageFormat imageFormat, Definition definition)
+        {
+            PDFFile pdfFile = PDFFile.Open(pdfInputPath);
+            if (!Directory.Exists(imageOutputPath))
+            {
+                Directory.CreateDirectory(imageOutputPath);
+            }
+            // validate pageNum
+            if (startPageNum <= 0)
+            {
+                startPageNum = 1;
+            }
+            if (endPageNum > pdfFile.PageCount)
+            {
+                endPageNum = pdfFile.PageCount;
+            }
+            if (startPageNum > endPageNum)
+            {
+                int tempPageNum = startPageNum;
+                startPageNum = endPageNum;
+                endPageNum = startPageNum;
+            }
+            // start to convert each page
+            for (int i = startPageNum; i <= endPageNum; i++)
+            {
+                Bitmap pageImage = pdfFile.GetPageImage(i - 1, 56 * (int)definition);
+                pageImage.Save(imageOutputPath + imageName + i.ToString() + "." + imageFormat.ToString(), imageFormat);
+                pageImage.Dispose();
+                SetTextMessage(100 * i / endPageNum);
+            }
+            pdfFile.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;
+        }
+        private void Form1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            //if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            //{
+            //    e.Effect = DragDropEffects.Link;
+            //}
+            //else
+            //{
+            //    e.Effect = DragDropEffects.None;
+            //}
+        }
+
+        
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string filePath = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            FileInfo fi = new FileInfo(filePath.ToString());
+            string dirName = fi.Name.Replace(".pdf", "");
+            saveDirName = System.Environment.CurrentDirectory + "\\" + dirName + "\\";
+            Thread fThread = new Thread(new ParameterizedThreadStart(SleepT) );//开辟一个新的线程
+            fThread.Start(filePath);
+        }
+
+
+         void SleepT(object filePath)
+        {
+            FileInfo fi = new FileInfo(filePath.ToString());
+            string dirName = fi.Name.Replace(".pdf", "");
+            Directory.CreateDirectory(dirName);
+            ConvertPDF2Image(filePath.ToString(), saveDirName, "NImage", 1, 500, ImageFormat.Png, Definition.Five);
+            MessageBox.Show("转换成功！");
+            btnConvert.Enabled = true;
+            if (saveDirName == "")
+            {
+                System.Diagnostics.Process.Start("explorer.exe",
+                    System.Environment.CurrentDirectory + "\\" + dirName + "\\");
+            }
+            else
+            {
+                System.Diagnostics.Process.Start("explorer.exe", saveDirName);
+            }
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void process1_Exited(object sender, EventArgs e)
+        {
+
+        }
+
+        public string pdfFileName;
+        public string saveDirName;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "请选择PDF文件";
+            dialog.Filter = "Pdf Files|*.pdf";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pdfFileName= dialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择保存文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                saveDirName = dialog.SelectedPath+"\\";
+            }
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(pdfFileName))
+            {
+                MessageBox.Show(this, "请选择转换的文件", "提示");
+                return;
+            }
+            if (string.IsNullOrEmpty(saveDirName))
+            {
+                MessageBox.Show(this, "请选择转换后保存的文件夹", "提示");
+                return;
+            }
+            btnConvert.Enabled = false;
+            Thread fThread = new Thread(new ParameterizedThreadStart(SleepT));//开辟一个新的线程
+            fThread.Start(pdfFileName);
+
+        }
+    }
+}
